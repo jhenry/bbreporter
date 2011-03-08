da7e2d08953627291bcab8c8a9100bb85a2ada81
Justin Henry <justin.henry@gmail.com>
2011-03-08 17:08:45 -0500

    Refactored reporting methods into an object for easier method access.

--- bbreporting.py
+++ bbreporting.py
@@ -5,114 +5,119 @@ import sys
 #import cx_Oracle
 from socket import socket
 
-CARBON_SERVER = '127.0.0.1'
-CARBON_PORT = 2003
+class BbReporting:
 
-def buildQueryByTermAndRole(term, role):
-  query = """select distinct(users.email) 
-      from course_main,course_users,users, activity_accumulator 
-      where course_users.crsmain_pk1=course_main.pk1 
-      and course_main.pk1=activity_accumulator.course_pk1 
-      and course_users.users_pk1=users.pk1 
-      and course_users.row_status='0' 
-      and course_users.role='""" + role + """' 
-      and course_main.row_status='0' 
-      and course_main.course_id LIKE '""" + term + "%'"
-  
-  return query 
+  def __init__(self):
+    self.CARBON_SERVER = '127.0.0.1'
+    self.CARBON_PORT = 2003
 
-def buildCourseQueryByTerm(term):
-  query = """select count(course_main.course_id)
-      from activity_accumulator, course_main, course_users
-      where activity_accumulator.course_pk1 = course_main.pk1
-      and course_users.crsmain_pk1=course_main.pk1
-      and course_main.course_id like '""" + term + """%'
-      and course_users.role='S'
-      group by course_main.course_id"""
-  
-  return query 
+  def buildQueryByTermAndRole(self, term, role):
+    query = """select distinct(users.email) 
+        from course_main,course_users,users, activity_accumulator 
+        where course_users.crsmain_pk1=course_main.pk1 
+        and course_main.pk1=activity_accumulator.course_pk1 
+        and course_users.users_pk1=users.pk1 
+        and course_users.row_status='0' 
+        and course_users.role='""" + role + """' 
+        and course_main.row_status='0' 
+        and course_main.course_id LIKE '""" + term + "%'"
+    
+    return query 
 
-def countInstructorsByTerm(term):
-  instructors = 0
-  query = buildQueryByTermAndRole(term, "P")
-  # connect to db and run query
-  return instructors
+  def buildCourseQueryByTerm(self, term):
+    query = """select count(course_main.course_id)
+        from activity_accumulator, course_main, course_users
+        where activity_accumulator.course_pk1 = course_main.pk1
+        and course_users.crsmain_pk1=course_main.pk1
+        and course_main.course_id like '""" + term + """%'
+        and course_users.role='S'
+        group by course_main.course_id"""
+    
+    return query 
 
-def countStudentsByTerm(term):
-  students = 0
-  query = buildQueryByTermAndRole(term, "S")
-  # connect to db and run query
-  return students
+  def countInstructorsByTerm(self, term):
+    instructors = 0
+    query = self.buildQueryByTermAndRole(term, "P")
+    # connect to db and run query
+    return instructors
 
-def countCoursesByTerm(term):
-  courses = 0
-  query = buildCourseQueryByTerm(term)
-  # connect to db and run query
-  return courses
+  def countStudentsByTerm(self, term):
+    students = 0
+    query = self.buildQueryByTermAndRole(term, "S")
+    # connect to db and run query
+    return students
 
-def buildReportPathByTerm(term, reportType):
-  path = "term." + term + "." + reportType 
-  return path
+  def countCoursesByTerm(self, term):
+    courses = 0
+    query =self.buildCourseQueryByTerm(term)
+    # connect to db and run query
+    return courses
 
-def buildReportByTermAndType(term, reportType, stamp=""):
-  if stamp == "":
-      stamp = int( time.time() )
-  
-  path = buildReportPathByTerm(term, reportType)
-  
-  if reportType == "courses":
-    result = countCoursesByTerm(term)
-  elif reportType == "instructors":
-    result = countInstructorsByTerm(term)
-  elif reportType == "students":
-    result = countStudentsByTerm(term)
-  
-  report =  path + " "+ str( result ) + " " + str( stamp )
-  
-  return report
+  def buildReportPathByTerm(self, term, reportType):
+    path = "term." + term + "." + reportType 
+    return path
 
-def runReports():
-  reports = []
-  
-  reports.append(buildReportByTermAndType("200909", "courses"))
-  reports.append(buildReportByTermAndType("200909", "students"))
-  reports.append(buildReportByTermAndType("200909", "instructors"))
-  
-  return reports
+  def buildReportByTermAndType(self, term, reportLabel, reportMethod, stamp=""):
+    if stamp == "":
+        stamp = int( time.time() )
+    
+    print 'locals: %s' % locals()
+    path = self.buildReportPathByTerm(term, reportLabel)
+    #result = locals()[reportMethod](term)
+    result = getattr(self, str(reportMethod))(term)
+    # if reportType == "courses":
+    #   result = countCoursesByTerm(term)
+    # elif reportType == "instructors":
+    #   result = countInstructorsByTerm(term)
+    # elif reportType == "students":
+    #   result = countStudentsByTerm(term)
+    
+    report =  path + " "+ str( result ) + " " + str( stamp )
+    
+    return report
 
-def sendReports(verbose = False, delay = 60):
-  carbon_socket = getCarbonConnection() 
-  while True:
-    full_report = '\n'.join(runReports()) + '\n' #all lines must end
-    if verbose == True:
-      printReports(full_report)
-    carbon_socket.sendall(full_report)
-    time.sleep(delay)
+  def runReports():
+    reports = []
+    
+    reports.append(buildReportByTermAndType("200909", "courses"))
+    reports.append(buildReportByTermAndType("200909", "students"))
+    reports.append(buildReportByTermAndType("200909", "instructors"))
+    
+    return reports
 
-def printReports(reports):
-    print "\nSending reports\n"
-    print '-------------------'
-    print reports
+  def sendReports(verbose = False, delay = 60):
+    carbon_socket = getCarbonConnection() 
+    while True:
+      full_report = '\n'.join(runReports()) + '\n' #all lines must end
+      if verbose == True:
+        printReports(full_report)
+      carbon_socket.sendall(full_report)
+      time.sleep(delay)
 
-def getCarbonConnection():
-  sock = socket()
-  try:
-    sock.connect( (CARBON_SERVER,CARBON_PORT) )
-  except:
-    print "Couldn't connect to localhost on port %d, is carbon-agent running?" % CARBON_PORT
-    sys.exit(1)
-  return sock
+  def printReports(reports):
+      print "\nSending reports\n"
+      print '-------------------'
+      print reports
 
-def getOracleConnection():
-  connection_string = DBConfig.USER + "/" + DBConfig.PASS + "@" + DBConfig.DATABASE
-  return cx_Oracle.connect(connection_string)
+  def getCarbonConnection():
+    sock = socket()
+    try:
+      sock.connect( (CARBON_SERVER,CARBON_PORT) )
+    except:
+      print "Couldn't connect to localhost on port %d, is carbon-agent running?" % CARBON_PORT
+      sys.exit(1)
+    return sock
 
-def sendQuery(query):
-  connection = getOracleConnection()
-  cursor = connection.cursor()
-  cursor.execute(query)
-  result = cursor.fetchall()[0][0]
-  cursor.close()
-  connection.close
-  return result
+  def getOracleConnection():
+    connection_string = DBConfig.USER + "/" + DBConfig.PASS + "@" + DBConfig.DATABASE
+    return cx_Oracle.connect(connection_string)
+
+  def sendQuery(query):
+    connection = getOracleConnection()
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()[0][0]
+    cursor.close()
+    connection.close
+    return result
 
--- test_bbreporting.py
+++ test_bbreporting.py
@@ -1,4 +1,4 @@
-import bbreporting
+from bbreporting import BbReporting
 import unittest
 import py
 import pytest_skipping
@@ -12,23 +12,33 @@ class BlackboardReports(unittest.TestCase):
 
   def testLoadConnectionConfigs(self):
     """should successfully import configurations"""
-    py.test.skip("pending")
+    self.pending() 
 
   def testConnectToDB(self):
     """successfully connect to the database"""
-    py.test.skip("pending")
+    self.pending() 
 
-  def testBuildQueryForExpoTool(self):
+  def testBuildQueryForExpoToolByTerm(self):
     """should create a query to count deployments of LO expo tool"""
-    py.test.skip("pending")
+    self.pending() 
+
+    bbreporting = BbReporting()
+    query = bbreporting.buildQueryForExpoToolByTerm(self.aValidTerm, role)
+    self.assertTrue(query.find(self.aValidTerm) > -1)
+    #self.assertTrue(query.find("role='P'") > -1)
 
   def testBuildQueryForNonCreditIccieYtd(self):
     """should create a query to count ICCIE users year-to-date (july 1)"""
-    py.test.skip("pending")
+    self.pending() 
+
+    bbreporting = BbReporting()
+    query = bbreporting.buildQueryForNonCreditIccieYtd(self.aValidTerm)
+    self.assertTrue(query.find(self.aValidTerm) > -1)
 
   def testBuildQueryForInstructorsByTerm(self):
     """should create query to countinstructors by term"""
     role = 'P'
+    bbreporting = BbReporting()
     query = bbreporting.buildQueryByTermAndRole(self.aValidTerm, role)
     self.assertTrue(query.find(self.aValidTerm) > -1)
     self.assertTrue(query.find("role='P'") > -1)
@@ -36,35 +46,38 @@ class BlackboardReports(unittest.TestCase):
   def testBuildQueryForStudentsByTerm(self):
     """should return query to count students by term"""
     role = 'S'
+    bbreporting = BbReporting()
     query = bbreporting.buildQueryByTermAndRole(self.aValidTerm, role)
     self.assertTrue(query.find(self.aValidTerm) > -1)
     self.assertTrue(query.find("role='S'") > -1)
 
   def testBuildQueryForCoursesByTerm(self):
     """should return query to select courses by term"""
+    bbreporting = BbReporting()
     query = bbreporting.buildCourseQueryByTerm(self.aValidTerm)
     self.assertTrue(query.find(self.aValidTerm) > -1)
  
   def testCountStudentsByTerm(self):
     """should return a number greater than or equal to 0"""
+    bbreporting = BbReporting()
     students = bbreporting.countStudentsByTerm(self.aValidTerm)
     self.assertTrue(students >= 0)
-    py.test.skip("pending")
   
   def testCountInstructorsByTerm(self):
     """should return a number greater than or equal to 0"""
+    bbreporting = BbReporting()
     instructors = bbreporting.countInstructorsByTerm(self.aValidTerm)
     self.assertTrue(instructors >= 0)
-    py.test.skip("pending")
  
   def testCountCoursesByTerm(self):
     """should return a number greater than or equal to 0"""
+    bbreporting = BbReporting()
     courses = bbreporting.countCoursesByTerm(self.aValidTerm)
     self.assertTrue(courses >= 0)
-    py.test.skip("pending")
   
   def testBuildReportPathByTerm(self):
     """should return a reporting path based on term, report type, and a timestamp"""
+    bbreporting = BbReporting()
     reportType = "courses"
     path = bbreporting.buildReportPathByTerm(self.aValidTerm, reportType)
       
@@ -73,36 +86,39 @@ class BlackboardReports(unittest.TestCase):
 
   def testBuildReport(self):
     """should return a report string consisting of a term-based path, a count, and a timestamp"""
-    reportType = "courses"
+    bbreporting = BbReporting()
+    reportMethodName = "countCoursesByTerm"
+    reportLabel = "courses"
     stamp = int( time.time() )
-    path = bbreporting.buildReportPathByTerm(self.aValidTerm, reportType)
-    report = bbreporting.buildReportByTermAndType(self.aValidTerm, reportType, stamp)
+    path = bbreporting.buildReportPathByTerm(self.aValidTerm, reportLabel)
+    report = bbreporting.buildReportByTermAndType(self.aValidTerm, reportLabel,
+        reportMethodName, stamp)
 
-    self.assertTrue(path.find("term." + self.aValidTerm + "." + reportType) !=
+    self.assertTrue(path.find("term." + self.aValidTerm + "." + reportLabel) !=
         -1)
+
     # match '200909.courses 1200 1294768918'
     report_pattern = re.compile ("term\.\d{6}\.\D+\s\d+\s\d{10}")
     self.assertTrue(report_pattern.match(report) != None)
   
   def testSendPastReports(self):
     """should prepare and send report data based on passed data"""
-    py.test.skip("pending")
+    self.pending() 
 	
   def testRunReports(self):
-    """should print a set of reports"""
-    py.test.skip("pending")
+    """should run a set of reports"""
+    self.pending() 
 
   def testBuildTermList(self):
     """should build a list of terms from a specified date until the present"""
-    py.test.skip("pending")
+    self.pending() 
     
   def testGetCurrentTerm(self):
     """should retrieve the current term code, based on the date"""
-    #pending() 
-    py.test.skip("pending")
+    self.pending() 
 
-  #def pending(self):
-    #py.test.skip("pending")
+  def pending(self, message = "pending"):
+    py.test.skip(message)
     
 if __name__ == "__main__":
     unittest.main()
